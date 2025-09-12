bf1e9a85e17d22cf32c04815b39876fd9a8f4f89
K.Deekshith <154054847+kuchanadeekshith@users.noreply.github.com>
2025-09-12 11:09:08 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ function checkURL() {
         return;
     }
 
-    fetch("http://127.0.0.1:8000/prediction", {
+    fetch("/prediction", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
